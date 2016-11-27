5fae21e2c2ea568006ab52d607701042322c8323
johnbdk <johnbdks@gmail.com>
2016-11-27 15:04:11 +0200

    Added logic for VRAM, testbench not working yet

---
+++ vgacontroller.v
@@ -0,0 +1,8 @@
+module vgacontroller(resetbutton, clk, VGA_RED, VGA_GREEN, VGA_BLUE, VGA_HSYNC, VGA_VSYNC);
+input resetbutton, clk;
+output VGA_RED, VGA_GREEN, VGA_BLUE, VGA_HSYNC, VGA_VSYNC;
+
+
+
+
+endmodule
