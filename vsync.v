77ca4f6c37252d629c1954b1f2ac70ac0030d821
johnbdk <johnbdks@gmail.com>
2016-12-13 23:24:00 +0200

    Sychronize vsync module with branch vgacontroller

--- vsync.v
+++ vsync.v
@@ -1,13 +1,18 @@
-module vsync(clk, reset, VPIXEL, VGA_VSYNC, RGB);
-input clk, reset;
-output [6:0]VPIXEL;
+module vsync(clk, reset, RGB_HSYNC, VPIXEL, VGA_VSYNC);
+/**
+  * O = 833500 cycles (scanline)
+  * P = 3200 cycles (pulse width)
+  * Q = 46400 cycles (back porch)
+  * R = 768000 cycles (display)
+  * S = 16000 cycles (front porch)
+  **/
+input  clk, reset, RGB_HSYNC;
+output [6:0] VPIXEL;
 output VGA_VSYNC;
-output RGB;
 
-reg [6:0]VPIXEL;
-reg [19:0]VSYNC_cnt;
+reg [6:0] VPIXEL;
+reg [19:0] VSYNC_cnt;
 reg [2:0] cnt;
-//reg [10:0]HPIXEL_next;
 
 always@(posedge clk or posedge reset)
 begin
@@ -17,11 +22,12 @@ begin
 		VSYNC_cnt = 20'd0;
 		cnt = 3'd0;
 	end
-	else
+	else if (RGB_HSYNC)
 	begin
-		if(VSYNC_cnt >= 49600 && VSYNC_cnt <= 817599)
+		if(VSYNC_cnt > 33 && VSYNC_cnt < 514)
 		begin
 			if(VPIXEL == 7'd95)
+			begin
 				if(cnt == 3'd4)
 				begin
 					VPIXEL = 7'd0;
@@ -29,34 +35,23 @@ begin
 				end
 				else
 					cnt = cnt + 1'b1;
-			else
-			begin
-				if(cnt == 3'd4)
+			end
+			else if(cnt == 3'd4)
 				begin
 					VPIXEL = VPIXEL + 1'b1;
 					cnt = 3'd0;
 				end
-				else
-					cnt = cnt + 1'b1;
-			end
+			else
+				cnt = cnt + 1'b1;
 		end
 
-
-		if(VSYNC_cnt == 20'd833499)
+		if(VSYNC_cnt == 524)
 			VSYNC_cnt = 20'd0;
-		else
+		else 
 			VSYNC_cnt = VSYNC_cnt+1;
 	end
 end
 
-assign RGB = (VSYNC_cnt >= 49600 && VSYNC_cnt <= 817599) ? 1'd1 : 1'd0;
-assign VGA_VSYNC = ((VSYNC_cnt >= 20'd0) && (VSYNC_cnt <= 20'd3199)) ? 1'd0 : 1'd1;
-/*
-O = 833500 cycles (scanline)
-P = 3200 cycles (pulse width)
-Q = 46400 cycles (back porch)
-R = 768000 cycles (display)
-S = 16000 cycles (front porch)
-*/
+assign VGA_VSYNC = !((VSYNC_cnt == 0) || (VSYNC_cnt == 1));
 
 endmodule
